ee48160871405f9155dd083e994d2a10ef6f776b
Jason Thomas <jayt1012520@yahoo.com>
2023-10-26 18:42:40 -0500

    Working

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include, re_path
 from django.views.static import serve
 
-from django.conf import setting
+from django.conf import settings
 
-
+from django.conf import settings
 
 from . import views
 
@@ -14,6 +14,11 @@ urlpatterns = [
         #serve media files when deployed
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
         #serve static files when deployed
+
+
+
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 
     path('movie_list/', views.MovieListView.as_view(), name='movie_list'),
     path('movie_detail/<int:pk>', views.MovieDetailView.as_view(), name='movie_detail'),
