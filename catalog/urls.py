3c529a0a655c59f0fcb41fdf68f77261dc968c6e
Todd Trost <68034370+ToddTrost@users.noreply.github.com>
2023-10-26 19:58:00 -0500

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,25 +1,15 @@
 from django.urls import path, include, re_path
 from django.views.static import serve
-
 from django.conf import settings
-
-from django.conf import settings
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-
         #serve media files when deployed
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
         #serve static files when deployed
-
-
-
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-
-
     path('movie_list/', views.MovieListView.as_view(), name='movie_list'),
     path('movie_detail/<int:pk>', views.MovieDetailView.as_view(), name='movie_detail'),
             ]
