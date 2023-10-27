7bf0dbd042cd710944234f4a29c6968b27bc4103
Todd Trost <68034370+ToddTrost@users.noreply.github.com>
2023-10-27 18:26:55 -0500

    Update urls.py
    Re-added in re-path after re-adding index that was deleted

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, re-path
+from django.views.static import serve
+from django.conf import settings
 from . import views
 
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':
+settings.MEDIA_ROOT}), #serve media files when deployed
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
+settings.STATIC_ROOT}), #serve static files when deployed
     path('', views.index, name='index'),
     path('movie_list/', views.MovieListView.as_view(), name='movie_list'),
     path('movie_detail/<int:pk>', views.MovieDetailView.as_view(), name='movie_detail'),
