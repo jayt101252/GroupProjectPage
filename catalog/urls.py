cd3926d15a6fdac6ab86e128f686a8d477a2bc62
ToddTrost <ttrost@unomaha.edu>
2023-11-20 01:46:01 -0600

    Fixed movieReview to moviereview
    Made changes Prof. Vlasnik pointed out to make URLs pages work with the html detail/list pages. Functionality is up!

--- catalog/urls.py
+++ catalog/urls.py
@@ -14,9 +14,8 @@ settings.STATIC_ROOT}), #serve static files when deployed
     path('movie_detail/<int:pk>', views.MovieDetailView.as_view(), name='movie_detail'),
     path('director_list/', views.DirectorListView.as_view(), name='director_list'),
     path('director_detail/<int:pk>', views.DirectorDetailView.as_view(), name='director_detail'),
-
-    path('moviereview_detail/<int:pk>', views.MovieReviewDetailView.as_view(), name='movieReview_detail'),
-    path('moviereview_list/', views.MovieReviewListView.as_view(), name='movieReview_list'),
+    path('moviereview_detail/<int:pk>', views.MovieReviewDetailView.as_view(), name='review_detail'),
+    path('moviereview_list/', views.MovieReviewListView.as_view(), name='moviereview_list'),
     path('moviereview/create/', views.WriteMovieReviewByUser.as_view(), name='write_review'),
     path('moviewreview/<int:pk>/update/', views.UpdateMovieReviewByUser.as_view(), name='update_review'),
     path('moviewreview/<int:pk>/delete/', views.moviereview_delete, name='review_delete'),
