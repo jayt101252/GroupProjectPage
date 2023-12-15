0cf44be92fc894ca61629d6c00999f3cfa51a66c
Todd Trost <68034370+ToddTrost@users.noreply.github.com>
2023-12-14 20:10:53 -0600

    Update settings.py

--- GroupProjectPage/settings.py
+++ GroupProjectPage/settings.py
@@ -135,4 +135,4 @@ LOGIN_REDIRECT_URL = '/'
 import os
 MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
